87969800ae7cb2d0a537f2b47d339e576d41bec1
Chris Davis <cjdisme@gmail.com>
2014-10-14 23:09:47 +0100

    Updates to HTML Validator

--- HtmlValidator.py
+++ HtmlValidator.py
@@ -2,13 +2,22 @@ import urllib.request
 import urllib.parse
 import re
 
-requestUrl = urllib.parse.quote('http://www.planetcjd.co.uk', '')
+def getValidStatus(requestUrl):
 
-html = urllib.request.urlopen('http://validator.w3.org/check?uri=' + requestUrl + '%2F&charset=%28detect+automatically%29&doctype=Inline')
+	requestUrl = urllib.parse.quote(requestUrl, '')
 
-validStatusRegex = re.search(r".*Errors found while checking this document.*", html.read().decode('utf-8'), re.MULTILINE)
+	html = urllib.request.urlopen('http://validator.w3.org/check?uri=' + requestUrl + '%2F&charset=%28detect+automatically%29&doctype=Inline')
+	htmlData = html.read().decode('utf-8')
 
-if (validStatusRegex):
-	print('invalid')
-else:	
-	print('valid')
+	invalidStatusRegex = re.search(r'.*Errors found while checking this document.*', htmlData, re.MULTILINE)
+
+	if (invalidStatusRegex):
+		errorCount = re.search(r'\d.*(?= Errors)', htmlData, re.MULTILINE)
+		warningCount = re.search(r'(?<=, )\d.*(?= warning\()', htmlData, re.MULTILINE)
+
+		return ('invalid', errorCount.group(0), warningCount.group(0))
+	else:	
+		return ('valid')
+
+# test function
+print(getValidStatus('http://www.microsoft.co.uk'))
