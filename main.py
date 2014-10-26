88252402eb8f038e7d899804466af3b78cc76734
Chris Davis <cjdisme@gmail.com>
2014-10-26 20:26:02 +0000

    putting it all together

---
+++ main.py
@@ -0,0 +1,26 @@
+import HtmlValidator
+import RandomWord
+import BingSearchResults
+import ServerSniffer
+import random
+
+testCount = 2
+bingAccountKey = BingSearchResults.getAccountKey()
+
+for x in range(0, testCount):
+
+	wordlist = RandomWord.loadWordList('/wordlist/wordlist.txt')
+	word = RandomWord.getRandomWord(wordlist)
+
+	searchUrls = BingSearchResults.parseSearchResults( BingSearchResults.getSearchResults(str(word), bingAccountKey) )
+
+	testUrl = random.choice(searchUrls)
+	server = ServerSniffer.sniffHeaders(testUrl)
+	validStatus = HtmlValidator.getValidStatus(testUrl)
+
+	print('Testing URL {0}\n Server: {1}\n validStatus: {2}'.format(testUrl, server, validStatus[0]))
+
+	if (validStatus[0] == 'invalid'):
+		print('Errors: {0} / Warnings: {1}'.format(validStatus[1], validStatus[2]))
+
+	print('\n\n')
