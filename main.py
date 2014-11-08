c5576604b3c953345ccc28f67133fdde1ac4776b
Chris Davis <cjdisme@gmail.com>
2014-11-08 17:13:11 +0000

    dont scrape duplicate domains

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import BingSearchResults
 import ServerSniffer
 import random
 import Logger
+import urllib.parse
 
 testCount = 10
 bingAccountKey = BingSearchResults.getAccountKey()
 wordlist = RandomWord.loadWordList('/wordlist/wordlist.txt')
 log = Logger.createLog()
+scrapedDomains = set()
 
 for x in range(0, testCount):
 
@@ -17,15 +19,26 @@ for x in range(0, testCount):
 	searchUrls = BingSearchResults.parseSearchResults( BingSearchResults.getSearchResults(str(word), bingAccountKey) )
 
 	testUrl = random.choice(searchUrls)
-	server = ServerSniffer.sniffHeaders(testUrl)
-	validStatus = HtmlValidator.getValidStatus(testUrl)
 
-	print('Testing URL {0}\n Server: {1}\n validStatus: {2}'.format(testUrl, server, validStatus[0]))
+	#check domain hasn't already been scraped
+	parsedTestUrl = urllib.parse.urlparse(testUrl)
+	testDomain = parsedTestUrl.netloc
 
-	if (validStatus[0] == 'invalid'):
-		print('Errors: {0} / Warnings: {1}'.format(validStatus[1], validStatus[2]))
-		Logger.writeLog(testUrl + ',' + server + ',' + 'invalid' + ',' + validStatus[1] + ',' + validStatus[2] + '\n',log)
-	else:
-		Logger.writeLog(testUrl + ',' + server + ',' + 'valid\n',log)
+	print('Checking if domain {0} has already been scraped...'.format(testDomain))
 
-	print('\n\n')
+	if (testDomain not in scrapedDomains):
+
+		scrapedDomains.add(testDomain)
+
+		server = ServerSniffer.sniffHeaders(testUrl)
+		validStatus = HtmlValidator.getValidStatus(testUrl)
+
+		print('Testing URL {0}\n Server: {1}\n validStatus: {2}'.format(testUrl, server, validStatus[0]))
+
+		if (validStatus[0] == 'invalid'):
+			print('Errors: {0} / Warnings: {1}'.format(validStatus[1], validStatus[2]))
+			Logger.writeLog(testUrl + ',' + server + ',' + 'invalid' + ',' + validStatus[1] + ',' + validStatus[2] + '\n',log)
+		else:
+			Logger.writeLog(testUrl + ',' + server + ',' + 'valid\n',log)
+
+		print('\n\n')
